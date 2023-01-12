ffacbfb4aeae7a6d54e0e1a9c6e1581ccd89233d
Mukhtor Allanazarov <121484379+mukhtorall@users.noreply.github.com>
2023-01-12 19:49:51 +0500

    Add files via upload

---
+++ src.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main();
+
+   int n,k;
+   cout<<" n ni kiriting ";  cin>>n;
+   cout<<" k ni kiriting ";  cin>>k;
+
+   forvppmnbsmdmovmohsmofdi
---
+++ src.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 30.09.2022.
+//
+
+#ifndef CASE_MASALALAR_SRC_H
+#define CASE_MASALALAR_SRC_H
+
+
+class src {
+
+};
+
+
+#endif //CASE_MASALALAR_SRC_H
